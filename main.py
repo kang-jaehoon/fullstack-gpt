86172f36d6b63b695ca2141be4d5dfe97cb613d8
captain-arthur <rltk777@naver.com>
2024-07-06 19:48:48 +0900

    init

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-from langchain.chat_models import Chat
+## LLM, ChatModel 두가지를 호출해보자.
+from langchain.llms.openai import OpenAI
+from langchain.chat_models import ChatOllama
 
+llm = OpenAI(model="http://localhost:3300/v1")
 
-print(Chat)
+a = llm.predict("How many planets are there?")
+
+print(a)
