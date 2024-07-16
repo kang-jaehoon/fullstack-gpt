83a5caf39553d34e512963154505e65881e7b6dd
captain-arthur <rltk777@naver.com>
2024-07-16 23:25:05 +0900

    gogo streamlit !!

---
+++ Home.py
@@ -0,0 +1,23 @@
+import streamlit as st
+
+st.set_page_config(
+  page_title="FullstackGPT Home",
+  page_icon="😅",
+)
+st.title("")
+
+with st.sidebar:
+  st.title("sidebar title")
+  st.text_input("xxx")
+
+
+tab_one, tab_two, tab_three = st.tabs(["A","B","C"])
+
+with tab_one:
+  st.write('a')
+  
+with tab_two:
+  st.write('b')
+  
+with tab_three:
+  st.write('c')
